172b1818f3d6f016fb0040e7a5ffcb9565b7d214
Gh0sTed <profbupeeli@gmail.com>
2024-03-13 21:21:46 +0300

    updated the header file

--- Project1/utils.h
+++ Project1/utils.h
@@ -9,8 +9,6 @@ bool is_opening_brace(char c);
 bool is_closing_brace(char c);
 char get_closing_brace(char c);
 char get_opening_brace(char c);
-bool is_matching_braces(const char& s, stack_t*& brace_stack);
 bool is_matching_braces(const char* str, stack_t*& brace_stack);
-bool is_matching_braces(const char& s, stack_t*& brace_stack, int& _counter, char &_val);
 
 #endif // !UTILS_H
