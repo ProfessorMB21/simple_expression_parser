0a04f8e09650f70039424727db3e18edda233764
Gh0sTed <profbupeeli@gmail.com>
2024-03-13 21:18:50 +0300

    update code

--- Project1/test.cpp
+++ Project1/test.cpp
@@ -11,12 +11,6 @@ int main()
 	char stack_val = NULL;	// to keep track of the popped item's value
 	stack_t *braces = new stack_t;
 
-	cout << c << " is brace: " << is_opening_brace(c) << endl;
-	cout << b << " is brace: " << is_opening_brace(b) << endl;
-	cout << "Corresp. clsoing brace to " << c << " is " << get_closing_brace(c) << endl;
-	cout << "Corresp. clsoing brace to " << b << " is " << get_closing_brace(b) << endl;
-	cout << endl;
-	
 	is_matching_braces(long_str, braces);
 
 	return 0;
